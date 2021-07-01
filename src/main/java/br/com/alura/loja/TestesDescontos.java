1f28b424e0ce656fcdec45039a27cb84ac068942
Robson <robmartinhao@gmail.com>
2021-07-01 19:39:47 -0300

    Aplicando o pattern Chain of Responsibility

--- src/main/java/br/com/alura/loja/TestesDescontos.java
+++ src/main/java/br/com/alura/loja/TestesDescontos.java
@@ -11,10 +11,9 @@ public class TestesDescontos {
 
     public static void main(String[] args) {
 
-        Orcamento orcamento = new Orcamento(new BigDecimal("200"), 4);
+        Orcamento orcamento = new Orcamento(new BigDecimal("1200"), 4);
         CalculadoraDeDescontos calculadora = new CalculadoraDeDescontos();
         System.out.println(calculadora.calcular(orcamento));
-
 
         Orcamento segundo = new Orcamento(new BigDecimal("800"), 5);
         System.out.println(calculadora.calcular(segundo));
--- src/main/java/br/com/alura/loja/desconto/CalculadoraDeDescontos.java
+++ src/main/java/br/com/alura/loja/desconto/CalculadoraDeDescontos.java
@@ -8,13 +8,11 @@ public class CalculadoraDeDescontos {
 
     public BigDecimal calcular(Orcamento orcamento) {
 
-        if (orcamento.getQuantidadeItens() > 5) {
-            return orcamento.getValor().multiply(new BigDecimal("0.1"));
-        }
+        Desconto desconto =
+                new DescontoParaOrcamentoComMaisDeCincoItens(
+                        new DescontoParaOrcamentoComValorMaiorQueQuinhentos(
+                                new SemDesconto()));
 
-        if (orcamento.getValor().compareTo(new BigDecimal("500")) > 0) {
-            return orcamento.getValor().multiply(new BigDecimal("0.1"));
-        }
-        return BigDecimal.ZERO;
+        return desconto.calcular(orcamento);
     }
 }

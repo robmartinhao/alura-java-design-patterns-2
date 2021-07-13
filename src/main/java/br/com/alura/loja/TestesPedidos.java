72e10cacc54bac8cca4032478ef83c0bf86b87d7
Robson <robmartinhao@gmail.com>
2021-07-13 18:49:43 -0300

    O processo de geração de um pedido

--- src/main/java/br/com/alura/loja/TestesPedidos.java
+++ src/main/java/br/com/alura/loja/TestesPedidos.java
@@ -3,6 +3,7 @@ package br.com.alura.loja;
 import br.com.alura.loja.pedido.GeraPedido;
 import br.com.alura.loja.pedido.GeraPedidoHandler;
 import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
+import br.com.alura.loja.pedido.acao.LogDePedido;
 import br.com.alura.loja.pedido.acao.SalvarPedidoNoBancoDeDados;
 
 import java.math.BigDecimal;
@@ -17,8 +18,9 @@ public class TestesPedidos {
 
         GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
         GeraPedidoHandler handler = new GeraPedidoHandler(
-            Arrays.asList(new SalvarPedidoNoBancoDeDados(),
-                          new EnviarEmailPedido())
+                Arrays.asList(new SalvarPedidoNoBancoDeDados(),
+                        new EnviarEmailPedido(),
+                        new LogDePedido())
         );
         handler.execute(gerador);
     }
